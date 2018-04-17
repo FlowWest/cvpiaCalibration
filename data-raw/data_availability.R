89ac45deb5d4f2acfd6d259f233173929123126d
thesadie <sgill@flowwest.com>
2018-04-17 12:20:34 -0700

    amer tuol data

--- data-raw/data_availability.R
+++ data-raw/data_availability.R
@@ -89,21 +89,76 @@ feather_tmp %>%
 
 feather_temp <- cdec_clean_temp(filter(feather_tmp, between(parameter_value, 40, 100)), 'FEATHER')
 
-feather_temp %>%
-  ggplot(aes(x = date, y = mean_temp_C, fill = as.factor(month(date)))) +
-  geom_col()
-
 use_data(feather_temp)
 
 # Tuolumne	2007	2017-----
 # USGS 11290000 TUOLUMNE R A MODESTO CA
 # Temperature, water, degrees Celsius 	 1965-07-21 	 2018-04-17
 # Discharge, cubic feet per second 	 1895-01-01 	 2018-04-16
+tuol_flw <- dataRetrieval::readNWISdv(siteNumbers = '11290000', parameterCd = '00060',
+                                      startDate = '2007-01-01', endDate = '2017-12-31')
+tuol_tmp <- dataRetrieval::readNWISdv(siteNumbers = '11290000', parameterCd = '00010',
+                                      startDate = '2007-01-01', endDate = '2017-12-31', statCd = c('00001', '00002', '00008'))
+
+tuol_flow <- usgs_clean_flow(tuol_flw, 'TUOLUMNE')
+
+ggplot(tuol_flow, aes(date, mean_flow_cfs)) + geom_line()
+
+use_data(tuol_flow)
+
+tuol_tmp %>%
+  select(Date, max_temp = X_00010_00001, min_temp = X_00010_00002, med_temp = X_00010_00008) %>%
+  group_by(year = year(Date), month = month(Date), day = days_in_month(Date)) %>%
+  summarise(mx = mean(max_temp, na.rm = TRUE), mn = mean(min_temp, na.rm = TRUE), md = mean(med_temp, na.rm = TRUE)) %>%
+  mutate(date = ymd(paste(year, month, day))) %>%
+  ungroup() %>%
+  select(-year, -month, -day) %>%
+  gather(stat, temp, -date) %>%
+  ggplot(aes(x=date, y=temp, color = stat)) +
+  geom_line()
+
+tuol_temp <- tuol_tmp %>%
+  select(Date, max_temp = X_00010_00001, min_temp = X_00010_00002, med_temp = X_00010_00008) %>%
+  group_by(year = year(Date), month = month(Date), day = days_in_month(Date)) %>%
+  summarise(mx = mean(max_temp, na.rm = TRUE), mn = mean(min_temp, na.rm = TRUE), md = mean(med_temp, na.rm = TRUE)) %>%
+  mutate(date = ymd(paste(year, month, day))) %>%
+  ungroup() %>%
+  select(-year, -month, -day) %>%
+  mutate(md = ifelse(is.nan(md), (mx + mn)/2, md), screw_trap = 'TUOLUMNE') %>%
+  select(date, mean_temp_C = md, screw_trap)
+
+use_data(tuol_temp)
 
 # American	2013	2016-----
 # USGS 11446500 AMERICAN R A FAIR OAKS CA
 # Temperature, water, degrees Celsius 	 1961-10-31 	 2018-04-17
 # Discharge, cubic feet per second 	 1904-10-01 	 2018-04-16
+amer_flw <- dataRetrieval::readNWISdv(siteNumbers = '11446500', parameterCd = '00060',
+                                      startDate = '2013-01-01', endDate = '2016-12-31')
+amer_tmp <- dataRetrieval::readNWISdv(siteNumbers = '11446500', parameterCd = '00010',
+                                      startDate = '2013-01-01', endDate = '2016-12-31', statCd = c('00001', '00002', '00008'))
+
+amer_flw %>%
+  ggplot(aes(Date, X_00060_00003)) +
+  geom_line()
+
+amer_flow <- usgs_clean_flow(amer_flw, 'AMERICAN')
+ggplot(amer_flow, aes(date, mean_flow_cfs)) + geom_col()
+
+use_data(amer_flow)
+
+amer_temp <- amer_tmp %>%
+  select(Date, max_temp = X_00010_00001, min_temp = X_00010_00002, med_temp = X_00010_00008) %>%
+  group_by(year = year(Date), month = month(Date), day = days_in_month(Date)) %>%
+  summarise(mx = mean(max_temp, na.rm = TRUE), mn = mean(min_temp, na.rm = TRUE), md = mean(med_temp, na.rm = TRUE)) %>%
+  mutate(date = ymd(paste(year, month, day))) %>%
+  ungroup() %>%
+  select(-year, -month, -day) %>%
+  mutate(md = ifelse(is.nan(md), (mx + mn)/2, md), screw_trap = 'AMERICAN') %>%
+  select(date, mean_temp_C = md, screw_trap)
+
+ggplot(amer_temp, aes(date, mean_temp_C)) + geom_col()
+use_data(amer_temp)
 
 # Battle	1998	2016------
 # USGS 11376550 BATTLE C BL COLEMAN FISH HATCHERY NR COTTONWOOD CA
